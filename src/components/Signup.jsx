a0d92c879040177c8e09427e619edb686258d57a
asthaishhh <asthaishhh@gmail.com>
2026-03-03 17:38:46 +0530

    extra bracket

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -31,7 +31,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}}/api/auth/register`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
